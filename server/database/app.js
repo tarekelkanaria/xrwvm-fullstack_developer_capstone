7693fc5054f3faa528e245990dd881cc6034a16e
tarekelkanaria <tarekelkanaria@gmail.com>
2026-04-20 10:04:07 +0200

    Modify app.js

--- server/database/app.js
+++ server/database/app.js
@@ -21,13 +21,13 @@ const Dealerships = require("./dealership");
 
 try {
   Reviews.deleteMany({}).then(() => {
-    Reviews.insertMany(reviews_data["reviews"]);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(() => {
-    Dealerships.insertMany(dealerships_data["dealerships"]);
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
 } catch (error) {
-  res.status(500).json({ error: "Error fetching documents" });
+  console.log(error);
 }
 
 // Express route to home
@@ -94,7 +94,7 @@ app.get("/fetchDealer/:id", async (req, res) => {
 app.post("/insert_review", express.raw({ type: "*/*" }), async (req, res) => {
   const data = JSON.parse(req.body);
   const documents = await Reviews.find().sort({ id: -1 });
-  let new_id = documents[0]["id"] + 1;
+  let new_id = documents[0].id + 1;
 
   const review = new Reviews({
     id: new_id,
